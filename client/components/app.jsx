0ddcf04e07ed3ef21067ce5a0250e0e0520e1456
Jerry <jerryong89@gmail.com>
2020-04-11 03:05:00 +0000

    added componentDidMount that calls getCartItems

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,6 +11,7 @@ export default class App extends React.Component {
       cart: []
     };
     this.setView = this.setView.bind(this);
+    this.getCartItems = this.getCartItems.bind(this);
   }
 
   setView(name, params) {
@@ -22,6 +23,10 @@ export default class App extends React.Component {
   getCartItems() {
     fetch('/api/cart')
       .then(res => res.json());
+  }
+
+  componentDidMount() {
+    this.getCartItems();
   }
 
   render() {
