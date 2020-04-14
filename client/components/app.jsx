05fd5aecd6bcf466b7ff4ee3d3ba5fb69718d081
Jerry <jerryong89@gmail.com>
2020-04-14 20:54:21 +0000

    added form and text box for user checkout information

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,6 +3,7 @@ import Header from './header';
 import ProductList from './product-list';
 import ProductDetails from './product-details';
 import CartSummary from './cart-summary';
+import CheckoutForm from './checkout-form';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -83,6 +84,7 @@ export default class App extends React.Component {
       <div>
         <Header changeItem={this.setView} cartItemCount={this.state.cart} />
         {conditionalRender}
+        <CheckoutForm />
       </div>
     );
   }
--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -11,7 +11,21 @@ export default class CheckoutFrom extends React.Component {
 
   render() {
     return (
-      <div></div>
+      <div className="container-fluid">
+        <h1>Checkout</h1>
+        {/* <div>{order.total}</div> */}
+        <form>
+          <div className="form-group">
+            <label>Name</label>
+            <input type="name" className="form-control"></input>
+            <label>Credit Card</label>
+            <input type="credit-card" className="form-control"></input>
+            <label>Shipping Address</label>
+            <input type="shipping-address" className="form-control"></input>
+            <button onClick={() => this.props.onRender('catalog', {})}> &lt; Continue Shopping</button><button className="btn btn-primary float-right">Place Order</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
