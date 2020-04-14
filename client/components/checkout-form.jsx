791b914d1b9fe23f12d9959d40a31dd8e13d24c5
Jerry <jerryong89@gmail.com>
2020-04-14 20:23:30 +0000

    defined checkoutform component with two states

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class CheckoutFrom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      creditCard: null
+    };
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
