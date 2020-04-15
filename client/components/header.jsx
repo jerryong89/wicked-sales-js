a2136d9a45d64d338fc36c8b4a1d6b61619e879f
Jerry <jerryong89@gmail.com>
2020-04-15 05:54:07 +0000

    modified header css to get rid of side scroll

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="row">
+      <div className="rowmod">
         <h3 className="header col-6">$ Wicked Sales</h3>
         <div className="header col-6"><div className="cart">{this.props.cartItemCount.length} Items &nbsp;<i onClick={() => this.props.changeItem('cart')} className="fas fa-shopping-cart cart"></i></div></div>
       </div>
