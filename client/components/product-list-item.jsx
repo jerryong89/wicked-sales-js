4cb7c64258b528ad7492baf69a1497ea57d726f4
Jerry <jerryong89@gmail.com>
2020-04-17 20:59:50 +0000

    added hover class to html and css

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ProductListItem(props) {
   const product = props.item;
   return (
-    <div onClick={() => props.changeItem('details', product.productId)} className="bottomM col-3">
+    <div onClick={() => props.changeItem('details', product.productId)} className="zoom bottomM col-3">
       <div ><img className="size" src={product.image} /></div>
       <div className="bottom font-weight-bold">{product.name}</div>
       <div className="bottom text-secondary">${(product.price / 100).toFixed(2)}</div>
