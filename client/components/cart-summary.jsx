5b6c73592706d2e142b63d8a61d0d322e9b07a39
Jerry <jerryong89@gmail.com>
2020-04-17 21:24:23 +0000

    added conditional rendering for checkout button

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -6,10 +6,13 @@ export default class CartSummary extends React.Component {
     const cartTotal = this.props.items.reduce((cur, acc) => cur + acc.price, 0).toFixed(2) / 100;
     const cartItem = this.props.items;
     let itemsInCart;
+    let checkOutButton;
     if (cartItem.length === 0) {
       itemsInCart = <h1>No Items In Cart</h1>;
+      checkOutButton = <div></div>;
     } else {
       itemsInCart = <h1>My Cart</h1>;
+      checkOutButton = <button className="btn btn-primary float-right lastRow" onClick={() => this.props.changeItem('checkout', {})}>Check Out</button>;
     }
     return (
       <div className="container">
@@ -19,7 +22,7 @@ export default class CartSummary extends React.Component {
         <div className="cartB"></div>
         <div>
           <div>Item Total: ${cartTotal}</div>
-          <button className="btn btn-primary float-right lastRow" onClick={() => this.props.changeItem('checkout', {})}>Check Out</button>
+          {checkOutButton}
         </div>
         <div className="cartSummaryB"></div>
       </div>
